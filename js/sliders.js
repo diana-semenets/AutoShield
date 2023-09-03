6adc8d3201cb3f2f731519293b3dbd433feb83bf
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-09-03 11:36:44 +0300

    Add files via upload

--- js/sliders.js
+++ js/sliders.js
@@ -68,3 +68,41 @@ const swiperWorks = new Swiper('.works__slider', {
     //autoplay: { delay: 5000 }
 });
 
+const swiperMain = new Swiper('.bg__slider', {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    speed: 500,
+    spaceBetween: 30,
+   
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            autoHeight: true,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        992: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+       
+    },
+
+    //autoplay: { delay: 5000 }
+});
+
+
